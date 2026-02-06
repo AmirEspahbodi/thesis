945aba47f65a08a9b9f306fe26b078237faeec0e
amir.espahbodi <dev.amir.espahbodi@gmail.com>
2026-02-06 10:34:29 +0330

    bug fix

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,10 @@
 """Models module for HSI few-shot learning."""
 
-from .backbone import HSI3DCNN, Conv3DBlock
+from .backbone import HSI3DCNN
 from .protonet import PrototypicalNetwork, build_prototypical_network
 
 __all__ = [
-    'HSI3DCNN',
-    'Conv3DBlock',
-    'PrototypicalNetwork',
-    'build_prototypical_network'
+    "HSI3DCNN",
+    "PrototypicalNetwork",
+    "build_prototypical_network",
 ]
